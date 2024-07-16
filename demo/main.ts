8fa0dc4692415f13fd38586fd7bc98137d57a09b
borderss <liepins01@gmail.com>
2024-07-17 02:59:49 +0300

    feat: types fix possibly

--- demo/main.ts
+++ demo/main.ts
@@ -1,6 +1,11 @@
 import '@/assets/main.css'
 
 import { createApp } from 'vue'
+import '../dist/style.css'
+import calendar from '../dist/vue-calendar'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(calendar)
+  .mount('#app')
